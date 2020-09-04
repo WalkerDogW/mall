9fe26cc381f85e9309966fb1f152333b1522ed0a
WalkerDogW <WalkerDogW@outlook.com>
2020-09-04 19:03:34 +0800

    category

--- product/src/test/java/site/javaee/mall/product/ProductApplicationTests.java
+++ product/src/test/java/site/javaee/mall/product/ProductApplicationTests.java
@@ -1,5 +1,7 @@
 package site.javaee.mall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import site.javaee.mall.product.entity.BrandEntity;
 import site.javaee.mall.product.service.BrandService;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -14,6 +19,25 @@ class ProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Test
+    void testOss() throws FileNotFoundException {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
+// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        String accessKeyId = "<yourAccessKeyId>";
+        String accessKeySecret = "<yourAccessKeySecret>";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 上传文件流。
+        InputStream inputStream = new FileInputStream("<yourlocalFile>");
+        ossClient.putObject("<yourBucketName>", "<yourObjectName>", inputStream);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
 
     @Test
     void contextLoads() {
@@ -28,8 +52,8 @@ class ProductApplicationTests {
 //        System.out.println(result);
 
         List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
-        list.forEach((item)->{
-            System.out.println( item);
+        list.forEach((item) -> {
+            System.out.println(item);
         });
 
     }
